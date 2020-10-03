94c2ac23b11b66e99bac92d9d62fd5ef004ea9df
Chrisando Pramudhita <chrsep@protonmail.com>
2020-10-03 21:21:13 +0700

    add a bunch if icons.

---
+++ src/pages/app/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react"
+import Head from "next/head"
+
+const App: FC = () => (
+  <div>
+    <Head>
+      <title>Clayton Prototype</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  </div>
+)
+
+export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import Head from "next/head"
 import React, { FC } from "react"
 
-const Home: FC = () => (
-  <div>
-    <Head>
-      <title>Clayton Prototype</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-  </div>
-)
+const Home: FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Clayton Prototype</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="px-3 leading-tight text-center text-4xl my-12 font-bold md:text-6xl max-w-lg mx-auto">
+        Learn Songs You Love, faster
+      </h1>
+
+      <div className="p-3 rounded bg-gray-300 m-3 text-black max-w-2xl mx-auto">
+        We are Open Source!
+      </div>
+    </div>
+  )
+}
 
 export default Home
