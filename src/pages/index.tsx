31e09a3c6187eb49f6c34c2d5fe61e5e5f3cb048
Chrisando Pramudhita <chrsep@protonmail.com>
2020-10-03 22:19:34 +0700

    redirect to app when logged in.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,45 @@
+/* eslint-disable react/no-danger */
 import Head from "next/head"
-import React, { FC, useEffect } from "react"
+import React, { FC } from "react"
 import { Svg } from "react-optimized-image"
-import { useRouter } from "next/router"
 import GithubIcon from "../icons/github.svg"
 import ChevronRight from "../icons/chevron_right.svg"
 import Header from "../components/Header/Header"
 
-const Home: FC = () => {
-  const router = useRouter()
-  useEffect(() => {
-    const cookies = document.cookie.split(";")
-    const isLoggedIn = cookies.findIndex((item) => item === "loggedIn=1")
-    if (isLoggedIn > -1) {
-      router.push("/app")
-    }
-  }, [])
+const Home: FC = () => (
+  <div>
+    <Head>
+      <title>Clayton Prototype</title>
+      <link rel="icon" href="/favicon.ico" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            const cookies = document.cookie.split(";")
+            const isLoggedIn = cookies.findIndex((item) => item === "loggedIn=1")
+            if (isLoggedIn > -1) {
+              window.location.href  = "/app"
+            }    
+        `,
+        }}
+      />
+    </Head>
 
-  return (
-    <div>
-      <Head>
-        <title>Clayton Prototype</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Header />
 
-      <Header />
+    <h1 className="px-6 leading-tight text-center text-4xl my-12 font-bold md:text-6xl max-w-lg mx-auto">
+      Learn Songs You Love, faster
+    </h1>
 
-      <h1 className="px-6 leading-tight text-center text-4xl my-12 font-bold md:text-6xl max-w-lg mx-auto">
-        Learn Songs You Love, faster
-      </h1>
-
-      <a href="https://github.com/chrsep/clayton">
-        <div className="p-3 rounded bg-gray-300 m-3 text-black max-w-2xl md:mx-auto mt-20">
-          <div className="flex items-center font-bold">
-            <Svg src={GithubIcon} className="w-5 h-5" />
-            <h2 className="ml-3">We are Open Source!</h2>
-            <Svg src={ChevronRight} className="w-8 h-8 ml-auto" />
-          </div>
+    <a href="https://github.com/chrsep/clayton">
+      <div className="p-3 rounded bg-gray-300 m-3 text-black max-w-2xl md:mx-auto mt-20">
+        <div className="flex items-center font-bold">
+          <Svg src={GithubIcon} className="w-5 h-5" />
+          <h2 className="ml-3">We are Open Source!</h2>
+          <Svg src={ChevronRight} className="w-8 h-8 ml-auto" />
         </div>
-      </a>
-    </div>
-  )
-}
+      </div>
+    </a>
+  </div>
+)
 
 export default Home
