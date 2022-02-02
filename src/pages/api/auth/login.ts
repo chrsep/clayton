84a901f48778838d02f16603f95c7ec8fbc6b84a
chris <chrsep@protonmail.com>
2022-02-02 16:47:09 +0700

    move to svelte kit.

--- src/pages/api/auth/login.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { newHandler } from "../../../handler"
-import { getUserAuthorizeRequestUri } from "../../../spotify/auth"
-
-const login = newHandler((req, res) => {
-  res.redirect(getUserAuthorizeRequestUri())
-})
-
-export default login
