1c70a2f80bdcd839eae8e67a01c6a2907cfec15f
chris <chrsep@protonmail.com>
2022-02-04 17:53:56 +0700

    try kv store.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit"
+
+  export const load: Load = async ({ params, fetch }) => {
+    const test = await process.env.KV_STORE.get("test")
+
+    return {
+      props: {
+        test,
+      },
+    }
+  }
+
+</script>
+
+<script lang="ts">
+  export let test = "asd";
+</script>
+
+<h1>Welcome to {test}</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
